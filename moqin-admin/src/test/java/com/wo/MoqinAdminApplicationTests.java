d07094e44b1f0ff0a8e0062273a0719e9a20b6c3
yang <wjk152153431@outlook.com>
2024-12-29 20:20:11 +0800

    测试：修改配置文件及测试连通性

--- moqin-admin/src/test/java/com/wo/MoqinAdminApplicationTests.java
+++ moqin-admin/src/test/java/com/wo/MoqinAdminApplicationTests.java
@@ -1,13 +1,25 @@
 package com.wo;
 
+import com.wo.domain.User;
+import com.wo.service.UserService;
+import com.wo.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
+
 @SpringBootTest
+@Slf4j
 class MoqinAdminApplicationTests {
+    @Resource
+    private UserService userService;
 
     @Test
     void contextLoads() {
+        R<User> userR = userService.selectByPrimaryKey(1L);
+        log.info(userR.toString());
     }
 
 }
